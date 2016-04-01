d1a35eb888b9f4e6f3d95c6f5bfacfedb974bf83
Ravinder Gill <quark2k1@gmail.com>
2016-04-01 08:51:50 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject webdev "0.1.0-SNAPSHOT"
+  :description "Web Development in Clojure"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring "1.4.0"]
+                 [compojure "1.5.0"]]
+
+  :min-lein-version "2.0.0"
+
+  :uberjar-name "webdev.jar"
+
+  :main webdev.core
+
+  :profiles {:dev
+             {:main webdev.core/-dev-main}})
+
+
