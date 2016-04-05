06cb4b00127da41bbdde1120809b971e5a0a14c8
Ravinder Gill <quark2k1@gmail.com>
2016-04-05 20:42:50 +0100

    About page, yo, calculator and floffy

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -2,26 +2,60 @@
   (:require [ring.adapter.jetty :as jetty]
             [ring.middleware.reload :refer [wrap-reload]]
             [compojure.core :refer [defroutes GET]]
-            [compojure.route :refer [not-found]]))
+            [compojure.route :refer [not-found]]
+            [ring.handler.dump :refer [handle-dump]]))
 
 (defn greet [req]
   {:status 200
-   :body "Hello World, now with compojure and no reload!"
-   :headers {}})
+   :body "Hello World, now with compojure and no reload!"})
+
+(defn about [req]
+  {:status 200
+   :body "By Quark Quark to learn how to write a web app in clojure."})
 
 (defn goodbye [req]
   {:status 200
-   :body "Tataaa!"
-   :headers{}})
+   :body "Tataaa!"})
+
+(defn request [req]
+  {:status 200
+   :body (pr-str req)})
+
+(defn yo [req]
+  (let [name (get-in req [:route-params :name])]
+    {:status 200
+     :body (str "Yo, " name "!")}))
+
+
+(def ops
+  {"+" +
+   "-" -
+   "*" *
+   ":" /})
+
+(defn calc [req]
+  (let [a (Integer. (get-in req [:route-params :a]))
+        b (Integer. (get-in req [:route-params :b]))
+        op (get-in req [:route-params :op])
+        f (get ops op)]
+    (if f
+    {:status 200
+     :body (str (f a b))}
+    {:status 404
+     :body (str "Unknown operator " op)})))
+
 
 (defn floffy [req]
   {:status 200
-   :body "Oh my, you found it!  Piggy Love Floffy, you know?! X x x X"
-   :headers {}})
+   :body "Oh my, you found it!  Piggy Love Floffy, you know?! X x x X"})
 
 (defroutes app
   (GET "/" [] greet)
+  (GET "/about" [] about)
   (GET "/goodbye" [] goodbye)
+  (GET "/request" [] handle-dump)
+  (GET "/yo/:name" [] yo)
+  (GET "/calc/:a/:op/:b" [] calc)
   (GET "/floffy" [] floffy)
   (not-found "Hmmm.... nothing here.  Maybe you can try something more special...!"))
 
